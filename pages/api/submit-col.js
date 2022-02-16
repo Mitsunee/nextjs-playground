d645b219c69781da4b14ee0bf775de83294e6bfc
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2022-02-16 13:40:07 +0100

    Test if you can write to a file in api route and read it in ISR

---
+++ pages/api/submit-col.js
@@ -0,0 +1,29 @@
+import { writeFile } from "@foxkit/node-util/fs";
+import { join } from "path";
+import { isCol } from "../../lib/isCol";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return;
+  }
+
+  const submittedColor = req.body.color;
+  console.log({ submittedColor });
+
+  try {
+    if (isCol(submittedColor)) {
+      await writeFile("stuff/savedColor.txt", submittedColor, "utf8");
+      res.status(200).json({ accepted: true, color: submittedColor });
+    } else {
+      res.status(400).end(`Bad Request`);
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).end(`Internal Server Error`);
+  }
+}
+
+export const config = {
+  unstable_includeFiles: ["stuff/savedColor.txt"]
+};
